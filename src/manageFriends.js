b4568a6070428c598149b7d10ab791cd3f4f7a3a
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-05-09 09:54:16 +0300

    Completed Instructions.

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,40 @@
-export function manageFriends(state, action) {
+let initialState = { friends: [] };
+export function manageFriends(state = initialState, action) {
   // your code here
+  switch (action.type) {
+    case "friends/add":
+      return { friends: [...state.friends, action.payload] };
+
+    case "friends/remove":
+      return { friends: state.friends.filter((friend) => friend.id !== action.payload) };
+
+    default:
+      return state;
+  }
 }
+
+// action = {
+//   type: "friends/add",
+//   payload: {
+//     name: "Chrome Boi",
+//     homewtown: "NYC",
+//     id: 1
+//   }
+// }
+
+// let remove = {
+//   type: "friends/remove",
+//   payload: 1,
+// };
+
+// let state = manageFriends(initialState, action)
+
+// console.log(initialState);
+
+// console.log(state);
+
+// state = manageFriends(initialState, remove)
+
+// console.log(state);
+
+
