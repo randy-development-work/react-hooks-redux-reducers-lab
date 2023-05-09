aeda549e57ec571be4df0558f789cf859e2f862d
Randy Kipkurui Cherutich <randy.kipkurui4@gmail.com>
2023-05-09 09:56:54 +0300

    Experiment with the reducer functions on the console.

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,12 +1,13 @@
 let initialState = { friends: [] };
 export function manageFriends(state = initialState, action) {
-  // your code here
   switch (action.type) {
     case "friends/add":
       return { friends: [...state.friends, action.payload] };
 
     case "friends/remove":
-      return { friends: state.friends.filter((friend) => friend.id !== action.payload) };
+      return {
+        friends: state.friends.filter((friend) => friend.id !== action.payload),
+      };
 
     default:
       return state;
@@ -36,5 +37,3 @@ export function manageFriends(state = initialState, action) {
 // state = manageFriends(initialState, remove)
 
 // console.log(state);
-
-
